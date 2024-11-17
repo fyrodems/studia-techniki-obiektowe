4b45bf3f83f10cf384e39623324aaf6666e14493
fyrodems <fyrodems@gmail.com>
2024-11-17 13:29:48 +0100

    sprzątaie

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-# from .views import LoginView
 
 from .views import login_view, logout_view, article_list_view, PublicArticleView, PrivateArticleView
 
 urlpatterns = [
-    path('login/', login_view, name='login'),  # Formularz logowania
-    path('logout/', logout_view, name='logout'),  # Wylogowanie
+    path('login/', login_view, name='login'),  
+    path('logout/', logout_view, name='logout'), 
     path('articles/public/', PublicArticleView.as_view(), name='public-articles'),
     path('articles/private/', PrivateArticleView.as_view(), name='private-articles'),
     path('articles/', article_list_view, name='article-list'),
